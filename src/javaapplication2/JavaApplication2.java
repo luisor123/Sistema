95b50583fe00ec015ed683ec51ac0c6bb6fb2926
luisor <luisorstaby@gmail.com>
2020-01-24 22:29:32 -0500

    nuevo-project

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistemajic;
+package javaapplication2;
 
 /**
  *
  * @author Luis Ortiz-PC
  */
-public class SistemaJIC {
+public class JavaApplication2 {
 
     /**
      * @param args the command line arguments
